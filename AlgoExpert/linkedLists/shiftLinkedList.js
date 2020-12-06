080a16b353185447ed169f4a954f9ad21d89c322
dannyhannyford <dannyhannyford@gmail.com>
2020-12-06 12:45:20 -0800

    accounts larger than the length of the LL

--- AlgoExpert/linkedLists/shiftLinkedList.js
+++ AlgoExpert/linkedLists/shiftLinkedList.js
@@ -1,4 +1,4 @@
-function shiftLinkedList(head, k) {
+function botchedshiftLinkedList(head, k) {
     if (k === 0) return head;
     let preShift = head;
     let curr = head;
@@ -44,6 +44,33 @@ function shiftLinkedList(head, k) {
         curr.next = preShift;
         return shiftHead;
     }
+  }
+
+  // if k is positive position of new tail (pnt) = length - k
+  // if k is negative pnt = |k|
+  function shiftLinkedList(head, k) {
+    // a guarenteed length of one in the problem
+    let listLength = 1;
+    let listTail = head;
+    while (listTail.next !== null) {
+        listTail = listTail.next;
+        listLength++
+    }
+
+    // modulo accounting for if k is longer than the length
+    const offset = Math.abs(k) % listLength;
+    if (offset === 0) return head;
+
+    const newTailPosition = k > 0 ? listLength - offset : offset;
+    let newTail = head;
+    for (let i = 1; i < newTailPosition; i++) {
+        newTail = newTail.next;
+    }
+
+    const newhead = newTail.next;
+    newTail.next = null;
+    listTail.next = head;
+    return newhead;
   }
   
   // This is the class of the input linked list.
