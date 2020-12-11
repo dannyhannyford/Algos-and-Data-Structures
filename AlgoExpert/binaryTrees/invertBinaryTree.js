b74d4b48609e75190daf394fe838ed194822f746
dannyhannyford <dannyhannyford@gmail.com>
2020-12-10 21:12:53 -0800

    invertBinaryTree complete

---
+++ AlgoExpert/binaryTrees/invertBinaryTree.js
@@ -0,0 +1,41 @@
+function invertBinaryTree(tree) {
+    // recursive
+    if (tree === null) {
+        return;
+    }
+    let temp = tree.right;
+    tree.right = tree.left;
+    tree.left = temp;
+
+    invertBinaryTree(tree.left);
+    invertBinaryTree(tree.right);
+    
+    return tree;
+}
+// T-O(n) S-O(depth)
+
+function invertBinaryTree(tree) {
+    // iterative
+    let queue = [tree];
+    while (queue.length > 0) {
+        let curr = queue.shift();
+        if (curr === null) continue;
+        let temp =  curr.left;
+        curr.left = curr.right;
+        curr.right = temp;
+        store.push(curr.left, curr.right);
+    }
+    return tree;
+  }
+
+// This is the class of the input binary tree.
+class BinaryTree {
+constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    }
+}
+
+// Do not edit the line below.
+exports.invertBinaryTree = invertBinaryTree;
