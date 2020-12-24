4221b3448bc823733a8a2f3b55d53d97d83803db
dannyhannyford <dannyhannyford@gmail.com>
2020-12-23 21:52:05 -0800

    memorizing dijkstras algo. Main part written out, need to create helper function

--- AlgoExpert/famous/dijkstrasAlgorithm.js
+++ AlgoExpert/famous/dijkstrasAlgorithm.js
@@ -1,6 +1,37 @@
 function dijkstrasAlgorithm(start, edges) {
+  const numberOfVertices = edges.length;
 
-  
+  const minDistances = [];
+  for (let i = 0; i < numberOfVertices; i++) {
+    minDistances.push(Infinity);
+  }
+  minDistances[start] = 0;
+
+  const visited = new Set();
+
+  while (visited.size != numberOfVertices) {
+    const [vertex, currentMinDistance] = getVertexWithMinDistance(minDistances, visited);
+
+    if (currentMinDistance === Infinity) {
+      break;
+    }
+    visited.add(vertex);
+
+    for (const edge of edges[vertex]) {
+      const [destination, distanceToDestination] = edge;
+
+      if (visitated.has(destination)) {
+        continue;
+      }
+
+      const newPathDistance = currentMinDistance + distanceToDestination;
+      const currentDestinationDistance = minDistances[destination];
+      if (newPathDistance < currentDestinationDistance) {
+        minDistances[destination] = newPathDistance;
+      }
+    }
+  }
+  return minDistances.map((x) => (x === Infinity ? -1 : x));
 }
 // T- O() S- O()
 
