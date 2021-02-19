29ded5da574af1afa18948b21a0c99e680f20cd0
dannyhannyford <dannyhannyford@gmail.com>
2021-02-19 15:43:33 -0800

    pseudo code

---
+++ AlgoExpert/arrays/moveElementToEnd.js
@@ -0,0 +1,31 @@
+function moveElementToEnd(array, toMove) {
+  // lead pointer
+  // backtracking pointer
+  
+  //while lead pointer index is smaller than backtracing index
+  // if lead pointer is equal to toMove
+    // check backtracing pointer
+      // while backtracing pointer is equal to toMove
+      // move it backwards
+
+
+    // swap lead and backtracing pointer
+    // move lead pointer forwards and backtracer backwards
+  
+  // return array
+}
+
+// Do not edit the line below.
+exports.moveElementToEnd = moveElementToEnd;
+
+
+
+// in place, any order
+// I- array, integer to move
+// 0- array
+// C- in place, T- 0(n) S-O(1)
+// E- number doesn't exist, 
+
+
+// 21222342
+// 13422222pe
