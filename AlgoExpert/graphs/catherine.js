fe95f245b0bdd7f42dd43edfe3d4fc3ddba8ad75
dannyhannyford <dannyhannyford@gmail.com>
2021-03-15 21:39:01 -0700

    graph attempts

---
+++ AlgoExpert/graphs/catherine.js
@@ -0,0 +1,17 @@
+// check for 0 length array
+//total = 0;
+
+// traversing array from left to right
+  // finding maximum area possible 
+  // storing these values in an array
+
+// traversing array from right to left 
+  // finding maximum area possible
+  // storing these values in an array
+
+// two arrays with the max values
+  //compare each index and their values
+    // take the smallest value for each index
+    // add them to total
+
+    // return total
---
+++ AlgoExpert/graphs/removeIslands.js
@@ -0,0 +1,45 @@
+// [
+//   [1, 0, 0, 0, 0, 0],
+//   [0, 1, 0, 1, 1, 1],
+//   [0, 0, 1, 0, 1, 0],
+//   [1, 1, 0, 0, 1, 0],
+//   [1, 0, 1, 1, 0, 0],
+//   [1, 0, 0, 0, 0, 1]
+// ]
+
+// [
+//   [1, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 1, 1, 1],
+//   [0, 0, 0, 0, 1, 0],
+//   [1, 1, 0, 0, 1, 0],
+//   [1, 0, 0, 0, 0, 0],
+//   [1, 0, 0, 0, 0, 1]
+// ]
+
+// look up,
+
+// if one and not border,
+
+//      repeat function
+
+// if 0,
+
+// go back and change path into 0's
+
+// can we go up?
+  // check if 1
+    // check if border
+      // go back to origin+1
+
+    // repeat checking function
+  // check if 0
+    // if we checked left, then change all visited squares to 0
+    // go to origin, start going right, down, left
+    
+// [u,r,d,l]
+// loop through urdl
+  // process here
+  // if 
+// variables 
+// current origin
+// current position
