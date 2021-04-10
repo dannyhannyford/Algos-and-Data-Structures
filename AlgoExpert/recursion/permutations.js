c3a74737039105fdae1d341591b999a569d67ea9
dannyhannyford <dannyhannyford@gmail.com>
2021-04-09 23:28:59 -0700

    permutations complete

---
+++ AlgoExpert/recursion/permutations.js
@@ -0,0 +1,20 @@
+function getPermutations(array) {
+  const perms = [];
+  const recurse = (index, array) => {
+    if(index === array.length -1) {
+      perms.push(array.slice());
+    } else {
+      for (let j = i; j < array.length; j++) {
+        [array[i],array[j]] = [ array[j], array[i]];
+        recurse(i+1, array);
+        [array[i],array[j]] = [ array[j], array[i]];
+      }
+    }
+  }
+  recurse(0, array);
+  return perms;
+}
+
+
+// Do not edit the line below.
+exports.getPermutations = getPermutations;
