c730804fe8a864d84a1958093aca3ea379f1493e
dannyhannyford <dannyhannyford@gmail.com>
2021-04-09 23:11:26 -0700

    doubleLinkedList complete

--- AlgoExpert/linkedLists/DoubleLInkedList.js
+++ AlgoExpert/linkedLists/DoubleLInkedList.js
@@ -37,8 +37,45 @@ class DoubleLinkedList {
   insertBefore(node, nodeToInsert) {
     // if node is head
     if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
-    
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node.prev;
+    nodeToInsert.next = node;
+    if(node.prev === null) {
+      this.head = nodeToInsert
+    } else {
+      node.prev.next = nodeToInsert
+    }
+    node.prev = nodeToInsert;
   }
+
+  insertAfter(node, nodeToInsert) {
+    if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node;
+    nodeToInsert.next = node.next;
+    if (node.next === null) {
+      this.tail = nodeToInsert;
+    } else {
+      node.next.prev = nodeToInsert;
+    }
+    node.next = nodeToInsert        
+  }
+
+  insertAtPosition(position, nodeToInsert) {
+    if (position === 1) {
+      this.setHead(nodeToInsert);
+      return;
+    }
+    let node = this.head;
+    let currentPosition = 1;
+    while (node !== null && currentPosition++ !== position) node = node.next;
+    if (node !== null) {
+      this.insertBefore(node, nodeToInsert);
+    } else {
+      this.setTail(nodeToInsert);
+    }
+  }
+  // traversing through until you find the value
   removeNodesWIthValue(value) {
     let node = this.head;
     while (node !== null) {
@@ -46,6 +83,27 @@ class DoubleLinkedList {
       node = node.next;
       if (nodeToRemove.value === value) this.remove(nodeToRemove)
     }
+  }
+  
+  // accounting for head and tail node,
+  remove(node) {
+    if (node === this.head) this.head = this.head.next;
+    if (node === this.tail) this.tail = this.tail.prev;
+    this.removeNodeBindings(node);
+  }
+  containsNodeWithValue(value) {
+    let node = this.head;
+    while (node.value !== value && node !== null) {
+      node = node.next;
+    }
+    return (node !== null);
+  }
+
+  removeNodeBindings(node) {
+    if (node.prev !== null) node.prev.next = node.next;
+    if (node.next !== null) node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
   }
 }
 
