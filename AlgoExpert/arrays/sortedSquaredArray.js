4f4596b9d896a60547c5d1c376d2ad05c4bb23da
dannyhannyford <dannyhannyford@gmail.com>
2021-05-02 17:40:28 -0700

    sortedSquare, looked up linear time solution

---
+++ AlgoExpert/arrays/sortedSquaredArray.js
@@ -0,0 +1,21 @@
+function sortedSquaredArray(array) {
+  // traverse array
+	const sortedSquares = new Array(array.length).fill(0);
+	let smallerValueIdx = 0;
+	let largerValueIdx = array.length - 1;
+	
+	for(let idx = array.length - 1; idx >= 0; idx--) {
+		const smallerValue = array[smallerValueIdx];
+		const largerValue = array[largerValueIdx];
+		if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+			sortedSquares[idx] = smallerValue*smallerValue;
+			smallerValueIdx++;
+		} else {
+			sortedSquares[idx] = largerValue*largerValue;
+			largerValueIdx--;
+		}
+	}
+	// value at index = value*value
+	
+  return sortedSquares;
+}
